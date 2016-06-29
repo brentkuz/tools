61bb82ffb632b5a0339eac40021c3081c69c317f
brentkuz <brentkuzmanich@gmail.com>
2016-06-28 20:44:56 -0700

    Added ListCopier

---
+++ Tools/GenericRepository.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web;
+
+namespace Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> Get(Expression<Func<T, bool>> filter = null,
+                            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null);
+        T GetByID(object id);
+        void Insert(T entity);
+        void Delete(object id);
+        void Delete(T entityToDelete);
+        void Update(T entityToUpdate);
+    }
+    public class GenericRepository<T> : IGenericRepository<T> where T : class 
+    {        
+        #region private
+        //DB context
+        internal DbContext context;
+        //DataSet for this
+        internal DbSet<T> dbSet;
+        #endregion
+
+        #region constructor
+        public GenericRepository(DbContext context)
+        {      
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+        #endregion
+
+        #region public
+        //Method to get a collection of the given entity
+        //In: Lambda expression for filtering (ex. q => q.Age > 25), Func with Lambda expression (ex. orderBy: q => q.OrderBy(d => d.Name))
+        //Out: Enumerable set of <TEntity>
+        public virtual IEnumerable<T> Get(Expression<Func<T, bool>> filter = null,
+                                                Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }    
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();               
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+        //Method to get an entity by id
+        //In: entity id; Out: obj <TEntity>
+        public virtual T GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+        //Method to insert a new obj <TEntity>
+        //In: entity obj; Out: n/a
+        public virtual void Insert(T entity)
+        {
+            dbSet.Add(entity);
+        }
+        //Method to initiate deletion if only id is provided
+        //In: entity id; Out: n/a
+        public virtual void Delete(object id)
+        {
+            T entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+        // Method to delete entity obj
+        //In: obj <TEntity>; Out: n/a
+        public virtual void Delete(T entityToDelete)
+        {            
+            dbSet.Attach(entityToDelete);            
+            dbSet.Remove(entityToDelete);
+        }
+        //Method to update entity obj
+        //In: obj <TEntity>; Out: n/a
+        public virtual void Update(T entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+        #endregion
+    }
+}
---
+++ Tools/SharePoint/ListCopier.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace SPListCopyConsole
+{
+    class ListCopier
+    {        
+        private ClientContext sourceContext;
+        private ClientContext targetContext;
+        private List sourceList;
+        private List targetList;
+        private Dictionary<string, string> columnMaps;
+        private Dictionary<string, Delegate> columnFormats; 
+
+        //ctor
+        public ListCopier(string sourcePath, string sourceName, string targetPath, string targetName)
+        {
+            this.sourceContext = new ClientContext(sourcePath);
+            this.targetContext = new ClientContext(targetPath);
+            this.sourceList = this.sourceContext.Web.Lists.GetByTitle(sourceName);
+            this.targetList = this.targetContext.Web.Lists.GetByTitle(targetName);
+            this.columnMaps = new Dictionary<string, string>();
+            this.columnFormats = new Dictionary<string, Delegate>();
+        }
+
+        #region public methods
+        //create mappings between list columns
+        public void AddColumnMaps(string col)
+        {
+            //split individual maps
+            string[] parts = col.Split(',');
+            foreach(string part in parts)
+            {
+                //split on '->'
+                var tmp  = part.Split(new string[]{"->"}, StringSplitOptions.None);
+                string sCol = tmp[0],
+                       tCol = tmp[1];
+                columnMaps.Add(tCol, sCol);                
+            }
+        }
+        public void AddColumnFormat(string targColName, Delegate del)
+        {
+            if(columnMaps[targColName] != null)
+            {
+                columnFormats[targColName] = del;
+            }
+        }
+
+        public void CopyListItems()
+        {
+            if(columnMaps.Count > 0)
+            {
+                CamlQuery query = CamlQuery.CreateAllItemsQuery();
+                ListItemCollection items = sourceList.GetItems(query);
+                sourceContext.Load(items);
+                sourceContext.ExecuteQuery();
+
+                //iterate and copy
+                foreach(ListItem item in items)
+                {
+                    ListItemCreationInformation createInfo = new ListItemCreationInformation();
+                    ListItem newItem = targetList.AddItem(createInfo);
+                    //map cols
+                    foreach(string col in columnMaps.Keys)
+                    {
+                        string txt = item[columnMaps[col]].ToString();
+                        //format col contents in target list
+                        if (columnFormats.ContainsKey(col))
+                        {
+                            var frmt = columnFormats[col];
+                            if (frmt != null)
+                            {
+                                //apply formatting
+                                txt = frmt.DynamicInvoke(txt).ToString();
+                            }
+                        }
+                        newItem[col] = txt;
+                    }
+                    
+                    newItem.Update();                    
+                    targetContext.ExecuteQuery();
+                    //notify copied item
+                    Console.WriteLine("Item: " + item.Id + " copied");
+                }
+            }
+        }
+        public void CloneList()
+        {
+
+        }
+        #endregion
+    }
+}
